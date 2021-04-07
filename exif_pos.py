b7524bba225a08c5efa8e547ed4b5e6ab76f078c
zsiki <siki1958@gmail.com>
2021-04-07 14:25:16 +0200

    use getexif method instead of protected _getexif

--- exif_pos.py
+++ exif_pos.py
@@ -1,22 +1,27 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
+"""
+    Generate coordinate list from exif info of images
+    (C) Zoltan Siki
+"""
+
+import sys
 import PIL.Image
 import PIL.ExifTags
-import sys
 
-def to_degrees(dir, value):
+def to_degrees(direction, value):
     """
     convert the GPS coordinates stored in the EXIF to degress in float format
     :param value: tuples of DMS
     :param dir: direction E/N/W/S
     :returns: decimal degree
     """
-    d = float(value[0][0]) / float(value[0][1])
-    m = float(value[1][0]) / float(value[1][1])
-    s = float(value[2][0]) / float(value[2][1])
-    w = 1 if dir in ('E', 'N') else -1
-    return w * (d + (m / 60.0) + (s / 3600.0))
+    angle_deg = float(value[0][0]) / float(value[0][1])
+    angle_min = float(value[1][0]) / float(value[1][1])
+    angle_sec = float(value[2][0]) / float(value[2][1])
+    angle_dir = 1 if direction in ('E', 'N') else -1
+    return angle_dir * (angle_deg + (angle_min / 60.0) + (angle_sec / 3600.0))
 
 def to_num(value):
     """
@@ -24,21 +29,20 @@ def to_num(value):
     :param value: tuple (int, int)
     :returns: float value
     """
-    return(value[0] / value[1])
+    return value[0] / value[1]
 
 def img_pos(name):
     """
     get GPS position from image
     :param name: image path
     :returns: tuple of (lat, lon)
-    """ 
-    img = PIL.Image.open(name)
-    exif = img._getexif()
+    """
+    with PIL.Image.open(name) as img:
+        exif = img.getexif()
     exif_data = {}
-    if exif:
-        exif_data = {
-            PIL.ExifTags.TAGS[k]: v for k, v in exif.items()
-                if k in PIL.ExifTags.TAGS }
+    if exif is not None:
+        exif_data = {PIL.ExifTags.TAGS[k]: v for k, v in exif.items()
+                     if k in PIL.ExifTags.TAGS}
     if 'GPSInfo' not in exif_data:
         return None
     return (to_degrees(exif_data['GPSInfo'][3], exif_data['GPSInfo'][4]),
@@ -48,11 +52,11 @@ def img_pos(name):
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Usage: {} image_file(s)".format(sys.argv[0]))
-        exit(-1)
+        sys.exit(1)
     # process parameters
-    for name in sys.argv[1:]:
-        p = img_pos(name)
+    for iname in sys.argv[1:]:
+        p = img_pos(iname)
         if p:
-            print("{},{:.6f},{:.6f},{:.2f}".format(name, p[0], p[1], p[2]))
+            print("{},{:.6f},{:.6f},{:.2f}".format(iname, p[0], p[1], p[2]))
         else:
-            print("{}, NULL, NULL, NULL".format(name))
+            print("{}, NULL, NULL, NULL".format(iname))
