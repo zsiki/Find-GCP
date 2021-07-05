95ab5b6b9e43c1eed2e50dc0c0cb0052d4339654
zsiki <siki1958@gmail.com>
2021-07-05 15:00:51 +0200

    missing import glob added

--- gcp_find.py
+++ gcp_find.py
@@ -14,6 +14,7 @@
 import sys
 import os
 import time
+import glob
 import argparse
 import numpy as np
 import matplotlib.pyplot as plt
@@ -403,7 +404,7 @@ if __name__ == "__main__":
     # parse command line arguments
     args = parser.parse_args()
     if os.name == "nt":
-        # extend wildcards on indows 
+        # extend wildcards on indows
         names = []
         for name in args.names:
             names += glob.glob(name)
