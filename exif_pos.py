848ae30ef9a6de02aadfa3bc43b37211f14c3bf8
zsiki <siki1958@gmail.com>
2022-04-08 08:46:06 +0200

    fix #15 update exif_pos to PIL version 9

--- exif_pos.py
+++ exif_pos.py
@@ -18,9 +18,12 @@ def to_degrees(direction, value):
     :param dir: direction E/N/W/S
     :returns: decimal degree
     """
-    angle_deg = float(value[0][0]) / float(value[0][1])
-    angle_min = float(value[1][0]) / float(value[1][1])
-    angle_sec = float(value[2][0]) / float(value[2][1])
+    try:
+        angle_deg = float(value[0][0]) / float(value[0][1])
+        angle_min = float(value[1][0]) / float(value[1][1])
+        angle_sec = float(value[2][0]) / float(value[2][1])
+    except:
+        angle_deg, angle_min, angle_sec = value
     angle_dir = 1 if direction in ('E', 'N') else -1
     return angle_dir * (angle_deg + (angle_min / 60.0) + (angle_sec / 3600.0))
 
@@ -30,7 +33,11 @@ def to_num(value):
     :param value: tuple (int, int)
     :returns: float value
     """
-    return value[0] / value[1]
+    try:
+        ret = value[0] / value[1]
+    except:
+        ret = float(value)
+    return ret
 
 def img_pos(name):
     """
@@ -39,16 +46,21 @@ def img_pos(name):
     :returns: tuple of (lat, lon)
     """
     with PIL.Image.open(name) as img:
-        exif = img.getexif()
-    exif_data = {}
+        try:
+            exif = img._getexif()
+        except AttributeError:
+            exif = None
+    ret = []
     if exif is not None:
         exif_data = {PIL.ExifTags.TAGS[k]: v for k, v in exif.items()
                      if k in PIL.ExifTags.TAGS}
-    if 'GPSInfo' not in exif_data:
-        return exif_data['DateTime'],
-    return (to_degrees(exif_data['GPSInfo'][3], exif_data['GPSInfo'][4]),
-            to_degrees(exif_data['GPSInfo'][1], exif_data['GPSInfo'][2]),
-            to_num(exif_data['GPSInfo'][6]), exif_data['DateTime'])
+        if 'GPSInfo' in exif_data and len(exif_data['GPSInfo']) > 6:
+            ret = [to_degrees(exif_data['GPSInfo'][3], exif_data['GPSInfo'][4]),
+                   to_degrees(exif_data['GPSInfo'][1], exif_data['GPSInfo'][2]),
+                   to_num(exif_data['GPSInfo'][6])]
+        if 'DateTime' in exif_data:
+            ret.append(exif_data['DateTime'])
+    return ret
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -57,7 +69,11 @@ if __name__ == "__main__":
     # process parameters
     for iname in sys.argv[1:]:
         p = img_pos(iname)
-        if len(p) > 1:          # position found?
+        if len(p) > 3:          # position and datetime found?
             print("{},{:.6f},{:.6f},{:.2f},{}".format(iname, p[0], p[1], p[2], p[3]))
-        else:
+        elif len(p) == 3:       # position found?
+            print("{},{:.6f},{:.6f},{:.2f}".format(iname, p[0], p[1], p[2]))
+        elif len(p) == 1:       # datetiem found?
             print("{},,,,{}".format(iname, p[0]))
+        else:
+            print("{},,,,,No EXIF".format(iname))
